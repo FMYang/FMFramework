78b0eeb61247f2de0fc1bbaed22df3f4be339f46
杨方明 <imyangfm@gmail.com>
2020-08-31 20:47:39 +0800

    init pod

---
+++ FMFramework/FMTestClass.m
@@ -0,0 +1,17 @@
+//
+//  FMTestClass.m
+//  Example
+//
+//  Created by yfm on 2020/8/31.
+//  Copyright © 2020 yfm. All rights reserved.
+//
+
+#import "FMTestClass.h"
+
+@implementation FMTestClass
+
++ (void)method1 {
+    NSLog(@"method1");
+}
+
+@end
