c7e6a874419538d466d2abbb291b4bed7ac67457
杨方明 <imyangfm@gmail.com>
2020-08-31 22:04:24 +0800

    upgrade repo

---
+++ Example/Pods/FMFramework/FMFramework/FMTestClass.h
@@ -0,0 +1,22 @@
+//
+//  FMTestClass.h
+//  Example
+//
+//  Created by yfm on 2020/8/31.
+//  Copyright © 2020 yfm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMTestClass : NSObject
+
++ (void)method1;
+
+// version 2
++ (void)method2;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/Pods/FMFramework/FMFramework/FMTestClass.m
@@ -0,0 +1,21 @@
+//
+//  FMTestClass.m
+//  Example
+//
+//  Created by yfm on 2020/8/31.
+//  Copyright © 2020 yfm. All rights reserved.
+//
+
+#import "FMTestClass.h"
+
+@implementation FMTestClass
+
++ (void)method1 {
+    NSLog(@"method1");
+}
+
++ (void)method2 {
+    NSLog(@"new method method2");
+}
+
+@end
